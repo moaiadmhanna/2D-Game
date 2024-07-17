c40f2c03ed461cd8787cca216e9e6ac40a5460d0
unknown <moaiadmhana999@.gmail.com>
2024-07-17 13:14:41 +0200

    added generating script for meterors

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -4,12 +4,10 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_timer_timeout():
-	pass # Replace with function body.
+# 1. we need to load the scene:
+var meteor_scene : PackedScene = load("res://Scenes/meteor.tscn")
+func _on_meteor_timer_timeout():
+	# 2. create an instance:
+	var meteor = meteor_scene.instantiate()
+	# 3. attach node to the current tree:
+	$Meteros.add_child(meteor)
--- Scenes/meteor.gd
+++ Scenes/meteor.gd
@@ -1,6 +1,16 @@
 extends Area2D
 
-
+func _ready():
+	var randomNumber := RandomNumberGenerator.new()
+	# start Poisiton:
+	var width = get_viewport().get_visible_rect().size[0]
+	var xPosition = randomNumber.randi_range(0,width)
+	var yPoistion = randomNumber.randi_range(-50,-150)
+	position = Vector2(xPosition,yPoistion)
+	
+func _process(delta):
+	position += Vector2(0,1.0) * 400 * delta
+	
 func _on_body_entered(body):
 	body.health -= 10;
 	body.health = clamp(body.health,0,100)
